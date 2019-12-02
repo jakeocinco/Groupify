fbfe558e08b0e78a465c1c838817e8cf44a6244e
jakeocinco <jakeocinco@me.com>
2019-12-02 09:09:44 -0500

    Initial Commit

---
+++ Groupify-SwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Groupify-SwiftUi
+//
+//  Created by Jacob Carlson on 12/2/19.
+//  Copyright © 2019 Jacob Carlson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
